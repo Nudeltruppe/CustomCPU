077cfc18d255c8924e5f2259fcd885fea54422e5
Glowman554 <glowman434@gmail.com>
2022-05-07 18:39:48 +0200

    update

--- microcode.c
+++ microcode.c
@@ -25,13 +25,13 @@
 
 
 uint8_t microcode[256][3] = {
-	{ 0, 0, 0 },
+	{ 0, 0, 0 }, // NOP
 	{ REG2_READ | REG2_TO_REG_WRITE_DATA, 0, REG1_WRITE }, // MOV <reg> <reg>
 	{ IMM16_TO_REG_WRITE_DATA, 0, REG1_WRITE }, // MOV <reg> <imm16>
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
+	{ 0, 0, 0 }, // OUT <imm16> <reg>
+	{ 0, 0, 0 }, // INP <imm16> <reg>
+	{ 0, 0, 0 }, // JNZ <imm16>
+	{ 0, 0, 0 }, // JNZ <reg>
 	{ REG1_READ | REG2_READ | ALU_ADD, 0, REG1_WRITE }, // ADD <reg> <reg>
 	{ REG1_READ | ALU_ADD, IMM16_TO_REG2_DATA, REG1_WRITE }, // ADD <reg> <imm16>
 	{ REG1_READ | REG2_READ | ALU_SUB, 0, REG1_WRITE }, // SUB <reg> <reg>
@@ -40,15 +40,15 @@ uint8_t microcode[256][3] = {
 	{ REG1_READ, ALU_NAND | IMM16_TO_REG2_DATA, REG1_WRITE }, // NAD <reg> <imm16>
 	{ REG1_READ | REG2_READ, ALU_NOR, REG1_WRITE }, // NOR <reg> <reg>
 	{ REG1_READ, ALU_NOR | IMM16_TO_REG2_DATA, REG1_WRITE }, // NOR <reg> <imm16>
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
-	{ 0, 0, 0 },
+	{ 0, 0, 0 }, // CMP <reg> <reg>
+	{ 0, 0, 0 }, // CMP <reg> <imm16>
+	{ 0, 0, 0 }, // JZR <imm16> 
+	{ 0, 0, 0 }, // JZR <reg>
+	{ 0, 0, 0 }, // LDR <reg> <imm16>
+	{ 0, 0, 0 }, // LDR <reg> <reg>
+	{ 0, 0, 0 }, // WTR <reg> <imm16>
+	{ 0, 0, 0 }, // WTR <reg> <reg> 
+	{ 0, 0, 0 }, // SWP <reg>
 	{ 0, IMM16_TO_REG2_DATA | REG2_TO_ADDRESS_BUS, PC_COUNT_WRITE }, // JMP <imm16>
 	{ REG2_READ, REG2_TO_ADDRESS_BUS, PC_COUNT_WRITE }, // JMP <reg>
 };
